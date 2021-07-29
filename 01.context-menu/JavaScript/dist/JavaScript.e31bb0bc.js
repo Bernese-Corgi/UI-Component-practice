253702b8d10929cf56642bbed1c1462030dd7b89
Bernese-Corgi <jnyng0621@gmail.com>
2021-07-29 17:35:37 +0900

    context-menu] body의 이벤트 리스너에 모든 이벤트 등록해서 이벤트 리스너 등록을 최소화하는 방안 및 문제점 분석

--- 01.context-menu/JavaScript/dist/JavaScript.e31bb0bc.js
+++ 01.context-menu/JavaScript/dist/JavaScript.e31bb0bc.js
@@ -194,42 +194,70 @@ module.hot.accept(reloadCSS);
 
 require("./style.css");
 
-// 1. item들을 감싸고 있는 wrapper를 변수에 담는다.
 const wrapper = document.querySelector('.wrapper');
-const items = document.querySelectorAll('.item'); // 2. 기존에 item에 등록했던 이벤트 리스너를, wrapper에 등록한다.
-// wrapper가 포함하는 하위 엘리먼트들을 클릭하면 wrapper에 등록된 이벤트 리스너가 실행이 될 것이다.
-
-wrapper.addEventListener('click', function (e) {
-  // e.target : 현재 클릭한 대상
-  const targetElem = e.target; // 이벤트 확산 방지 (버블링 방지)
-
-  e.stopPropagation(); // 현재 클릭한 대상의 classList에 item 클래스가 없으면 아무 동작도 하지 않는다.
-  // = 현재 클릭한 대상이 item이 아니면 아무 동작도 하지 않는다.
-
-  if (!targetElem.classList.contains('item')) return; // if문 아래에서는 item 클래스를 가진 targetElem에 대해서만 동작된다.
-  // targetElem의 classList에 토글로 open 클래스를 준다.
-
-  targetElem.classList.toggle('open'); // item들에 대해 순회
-
-  items.forEach(function (elem) {
-    // 엘리먼트가 targetElem과 같지 않으면 open 클래스를 지운다.
-    if (elem !== targetElem) elem.classList.remove('open');
-  });
-}); // 3. 목록 외부를 클릭하면 사라지는 동작
-// document에 있는 body에 직접 리스너를 등록한다.
+const items = document.querySelectorAll('.item'); // // 2. 기존에 item에 등록했던 이벤트 리스너를, wrapper에 등록한다.
+// // wrapper가 포함하는 하위 엘리먼트들을 클릭하면 wrapper에 등록된 이벤트 리스너가 실행이 될 것이다.
+// wrapper.addEventListener('click', function (e) {
+//   // e.target : 현재 클릭한 대상
+//   const targetElem = e.target;
+//   // 이벤트 확산 방지 (버블링 방지)
+//   e.stopPropagation();
+//   // 현재 클릭한 대상의 classList에 item 클래스가 없으면 아무 동작도 하지 않는다.
+//   // = 현재 클릭한 대상이 item이 아니면 아무 동작도 하지 않는다.
+//   if (!targetElem.classList.contains('item')) return;
+//   // if문 아래에서는 item 클래스를 가진 targetElem에 대해서만 동작된다.
+//   // targetElem의 classList에 토글로 open 클래스를 준다.
+//   targetElem.classList.toggle('open');
+//   // item들에 대해 순회
+//   items.forEach(function (elem) {
+//     // 엘리먼트가 targetElem과 같지 않으면 open 클래스를 지운다.
+//     if (elem !== targetElem) elem.classList.remove('open');
+//   });
+// });
+//
 
 document.body.addEventListener('click', function (e) {
-  // e.target의 classList가 context를 가지고 있다면 아무일도 하지 않는다. (context가 열려있는 상태면 동작하지 않는다.)
-  if (e.target.classList.contains('context')) return; // context가 열려있는 상태 외의 item
+  const targetClassList = e.target.classList; // 1. e.target의 classList가 context를 가지고 있다면 아무 동작도 하지 않는다.
+
+  if (targetClassList.contains('context')) return; // 2. e.target의 classList가 item을 가지고 있다면 wrapper에 등록했던 이벤트 동작을 한다.
+
+  if (targetClassList.contains('item')) {
+    // e.target의 classList에 토글로 open 클래스를 준다.
+    targetClassList.toggle('open');
+    items.forEach(function (elem) {
+      // item이 e.target과 같지 않으면 open 클래스를 지운다.
+      console.log(elem, e.target);
+      if (elem !== e.target) elem.classList.remove('open');
+    });
+    return;
+  } // context가 열려있는 상태 외의 item
+
 
   items.forEach(function (elem) {
     // 목록 이외에 모든 부분을 어떤 부분을 선택하면 item에 있는 open 클래스를 전부 지워준다.
     elem.classList.remove('open');
   });
-}); // 이 코드를 추가하면 context가 열리지 않는 것처럼 보인다.
-// open이 안 되는 게 아니라 open됐다가 close가 바로 처리가 되는 것이다.
-// item -> wrapper -> body 순으로 이벤트가 전파(버블링)되고, 이벤트는 wrapper, body 에서 각각 발생해서 총 2번 이벤트가 발생하게 된다.
-// wrapper의 버블링을 더 이상 일어나지 않게 막아야한다.
+}); // 기서 한 번 잡아보자 타겟의 classList를 한번 변수를 먼저 잡아놓고 보겠습니다 이렇게 변수를 잡아놓으면 조건을 따지기가 쉬워지죠
+// contains로. 아까처럼 이거를 바꾸면 되겠죠.
+// 얘를 targetClassList가 context이면은.
+// 아무 동작도 하지 말고 다시 targetClassList가 item을 가지고 있다.
+// 라고 하면은 이때는 이 위에 있는 동작을 하면 되겠죠
+// targetClassList를 토글로 open을 톡을 시켜주고요 item들 forEach를 돌려서 이 부분이죠.
+// 가져가겠습니다
+// 네 elem과 e.target 비교하면 되겠죠 이 상태에서 여기서는 더 이상 동작을 하면 안 되니까 여기서는 return을 시켜주겠습니다.
+// 그러면 된 거 같은데요 지금 제가 어떤 작업을 했냐면 body 하나에만 클릭 이벤트를 구매를 하고선 context에 해당하는지 item에 해당하는지 혹은 그 외에 다른 어떤 것인지 네 따라서 처리를 다르게 한 겁니다 context일 때는 아무 동작도 하지 않고 return에서 바로 끝내버리고 item에 다 할 때는 이제 해당 item에는 open 클리스를 넣어주고 나머지는 지워주는 작업 하고 종류 그렇지 않은 그외의 경우에는 전북 open 클래스를 지워라라는 내용이 되겠죠.
+// 타. 게 오타가 있네요 여기. 이것만 고쳐주면 될 것 같은데요 왜 딴 데를 클릭하면 사라지고 네 목표하는 바를 다 이뤘습니다.
+// 장점은 뭔가요. 리스너가 줄어든다는 거는 굉장한 장점입니다 단점이라고 하면 이 조건문들이 많아질 수밖에 없는 거죠.
+// 클릭 이벤트 하나의 대해서 조건을 계속 따져야 돼요 이 조건문을 최적화하는 방법을 고민할 필요가 있겠습니다 그리고 또 한 가지 문제는 이제 body 전체에다가 에디 이벤트 리스를 걸었기 때문에 이 페이지의 동작이 전부 종료되기 전까지는
+// 이 하나의 이벤트 리스너에 전적으로 의존할 수밖에 없습니다.
+// 그러니까 만약에 예전에 같은 경우는 이 wrapper라고 하는 거에 리스너를 걸어놓고.
+// body 리스너를 걸었을 때는 wrapper가 사라졌을 때.
+// 이 이벤트 리스 역시도 동작을 안 하게 되면서 최적화가 관리가 자동으로 될 수 있습니다.
+// 혹은 내가 이 wrapper에 대한 클릭 이벤트 감시를 중단하고 싶다라고 하면은 얘에 대해서만 remove 이벤트.
+// 리스너를 해주면. 됐어요 이런 식으로 메모리 관리를 할 수가 있는데 이 경우는
+// 내용이 하나의 전부 들어가 있기 때문에 개별적인 리스너들에 대한 에드 remove에 대한 관리가 될 수가 없는 문제가 있습니다.
+// 애들 장단점을 잘 따져서 앞에 먼저 방금 소개해 드렸던 wrapper의 클래스 레포의 클릭 이벤트를 걸고 body에도 클릭 이벤트를 거는 이 두 개의 클릭 이벤트를 이용하는 방법과 body에다가 하나만 거는거 방법이 두 가지 중에서 잘 고민해서 작업을 하시면 되겠습니다.
+// 실무에서는
 },{"./style.css":"style.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- 01.context-menu/JavaScript/index.js
+++ 01.context-menu/JavaScript/index.js
@@ -1,36 +1,49 @@
 import './style.css';
 
-// 1. item들을 감싸고 있는 wrapper를 변수에 담는다.
-const wrapper = document.querySelector('.wrapper');
+// const wrapper = document.querySelector('.wrapper');
 const items = document.querySelectorAll('.item');
 
-// 2. 기존에 item에 등록했던 이벤트 리스너를, wrapper에 등록한다.
-// wrapper가 포함하는 하위 엘리먼트들을 클릭하면 wrapper에 등록된 이벤트 리스너가 실행이 될 것이다.
-wrapper.addEventListener('click', function (e) {
-  // e.target : 현재 클릭한 대상
-  const targetElem = e.target;
+// // wrapper가 포함하는 하위 엘리먼트들을 클릭하면 wrapper에 등록된 이벤트 리스너가 실행이 될 것이다.
+// wrapper.addEventListener('click', function (e) {
+//   // e.target : 현재 클릭한 대상
+//   const targetElem = e.target;
 
-  // 이벤트 확산 방지 (버블링 방지)
-  e.stopPropagation();
+//   // 이벤트 확산 방지 (버블링 방지)
+//   e.stopPropagation();
 
-  // 현재 클릭한 대상의 classList에 item 클래스가 없으면 아무 동작도 하지 않는다.
-  // = 현재 클릭한 대상이 item이 아니면 아무 동작도 하지 않는다.
-  if (!targetElem.classList.contains('item')) return;
-  // if문 아래에서는 item 클래스를 가진 targetElem에 대해서만 동작된다.
-  // targetElem의 classList에 토글로 open 클래스를 준다.
-  targetElem.classList.toggle('open');
-  // item들에 대해 순회
-  items.forEach(function (elem) {
-    // 엘리먼트가 targetElem과 같지 않으면 open 클래스를 지운다.
-    if (elem !== targetElem) elem.classList.remove('open');
-  });
-});
+//   // 현재 클릭한 대상의 classList에 item 클래스가 없으면 아무 동작도 하지 않는다.
+//   // = 현재 클릭한 대상이 item이 아니면 아무 동작도 하지 않는다.
+//   if (!targetElem.classList.contains('item')) return;
+//   // if문 아래에서는 item 클래스를 가진 targetElem에 대해서만 동작된다.
+//   // targetElem의 classList에 토글로 open 클래스를 준다.
+//   targetElem.classList.toggle('open');
+//   // item들에 대해 순회
+//   items.forEach(function (elem) {
+//     // 엘리먼트가 targetElem과 같지 않으면 open 클래스를 지운다.
+//     if (elem !== targetElem) elem.classList.remove('open');
+//   });
+// });
 
-// 3. 목록 외부를 클릭하면 사라지는 동작
-// document에 있는 body에 직접 리스너를 등록한다.
+// body 하나에만 이벤트 리스너 등록
 document.body.addEventListener('click', function (e) {
-  // e.target의 classList가 context를 가지고 있다면 아무일도 하지 않는다. (context가 열려있는 상태면 동작하지 않는다.)
-  if (e.target.classList.contains('context')) return;
+  // e.target의 classList를 변수로 잡는다 - 변수를 잡아놓으면 조건을 따지기 쉬워진다.
+  const targetClassList = e.target.classList;
+
+  // 1. e.target의 classList가 context를 가지고 있다면 아무 동작도 하지 않는다.
+  if (targetClassList.contains('context')) return;
+
+  // 2. e.target의 classList가 item을 가지고 있다면 wrapper에 등록했던 이벤트 동작을 한다.
+  if (targetClassList.contains('item')) {
+    // e.target의 classList에 토글로 open 클래스를 준다.
+    targetClassList.toggle('open');
+
+    items.forEach(function (elem) {
+      // items를 순회하면서 item이 e.target에 해당하지 않는 item에서는 open 클래스를 지운다.
+      if (elem !== e.target) elem.classList.remove('open');
+    });
+    // 이 상태에서는 더이상 동작을 하면 안되므로 return 시킨다.
+    return;
+  }
 
   // context가 열려있는 상태 외의 item
   items.forEach(function (elem) {
@@ -38,7 +51,15 @@ document.body.addEventListener('click', function (e) {
     elem.classList.remove('open');
   });
 });
-// 이 코드를 추가하면 context가 열리지 않는 것처럼 보인다.
-// open이 안 되는 게 아니라 open됐다가 close가 바로 처리가 되는 것이다.
-// item -> wrapper -> body 순으로 이벤트가 전파(버블링)되고, 이벤트는 wrapper, body 에서 각각 발생해서 총 2번 이벤트가 발생하게 된다.
-// wrapper의 버블링을 더 이상 일어나지 않게 막아야한다.
+
+/*
+body 에 모든 이벤트를 등록해서 이벤트 리스너를 하나만 등록했을 때의 문제점
+
+- 조건문들이 많아진다. 
+  : 클릭 이벤트 하나에 대해서 조건을 계속 따져야 한다. 
+  ⇒ 조건문을 최적화하는 방법을 고민할 필요가 있다. 
+    
+- 메모리 관리가 어렵다. 
+  - body 전체에 addEventListner를 걸었기 때문에 페이지의 동작이 전부 종료되기 전까지는 body 하나의 이벤트 리스너에 전적으로 의존할 수밖에 없다. 
+  - 이벤트 리스너를 body 하나만 사용하게 되면, 내용이 하나에 전부 들어가 있기 때문에 개별적인 리스너들에 대한 `addEventListner`, removeEventListner에 대한 관리가 될 수가 없는 문제가 있다. 
+*/
